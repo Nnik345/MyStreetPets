3134d3c64da056768a102d0de1633a6fa241c5e4
Nnik345 <nikhilsivakumar123@gmail.com>
2025-01-17 16:39:51 +0530

    update to mongodb

--- my-react-app/src/Utils/fetchStreetDogs.js
+++ my-react-app/src/Utils/fetchStreetDogs.js
@@ -1,11 +1,10 @@
 export const fetchDogs = async () => {
-    try {
-      const response = await fetch("./Data/streetDogs.json");
-      if (!response.ok) throw new Error("Failed to fetch data");
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching dogs:", error);
-      return [];
-    }
-  };
-  
+  try {
+    const response = await fetch("https://mystreetpets.onrender.com/api/street-dogs");
+    if (!response.ok) throw new Error("Failed to fetch data");
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching dogs:", error);
+    return [];
+  }
+};
