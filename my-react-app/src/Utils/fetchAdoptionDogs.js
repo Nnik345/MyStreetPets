7ea252240d558a6653423154e6351c59107448d8
Nnik345 <nikhilsivakumar123@gmail.com>
2025-01-17 20:14:18 +0530

    Updated to AWS

--- my-react-app/src/Utils/fetchAdoptionDogs.js
+++ my-react-app/src/Utils/fetchAdoptionDogs.js
@@ -1,6 +1,6 @@
 export const fetchDogs = async () => {
   try {
-    const response = await fetch("https://mystreetpets.onrender.com/api/adoption-dogs");
+    const response = await fetch("https://k34176tah4.execute-api.ap-south-1.amazonaws.com/default/fetchAdoptionAnimals");
     if (!response.ok) throw new Error("Failed to fetch data");
     return await response.json();
   } catch (error) {
